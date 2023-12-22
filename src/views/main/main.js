e81ca19004dc98a3a70f7367049bd04cd797f98f
ZWRKje <andrey09767@gmail.com>
2023-12-22 17:48:54 +0300

    feat: add search

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,6 +1,7 @@
 import { AbstractView } from '../../common/view.js';
 import onChange from 'on-change';
 import { Header } from '../../components/header/header.js';
+import { Search } from '../../components/search/search.js';
 
 export class MainView extends AbstractView {
   state = {
@@ -13,17 +14,34 @@ export class MainView extends AbstractView {
     super();
     this.appState = appState;
     this.appState = onChange(this.appState, this.appStateHook.bind(this));
+    this.state = onChange(this.appState, this.stateHook.bind(this));
     this.setTitle('Поиск книг');
   }
 
   appStateHook(path) {
     if (path === 'favorites') {
-        console.log(path)
+      console.log(path);
     }
   }
 
+  async stateHook(path) {
+    if (path === 'searchQuery') {
+      this.state.loading = true;
+      console.log('a');
+      const data = await this.loadList(this.state.searchQuery, this.state.offset);
+      this.state.loading = false;
+      console.log(data);
+      this.state.list = data;
+    }
+  }
+
+  async loadList(q, offset) {
+    const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
+    return res.json();
+  }
   render() {
     const main = document.createElement('div');
+    main.append(new Search(this.state).render());
     this.app.innerText = '';
     this.app.append(main);
     this.renderHeader();
